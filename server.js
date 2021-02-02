c5cd3de265a9ebbf58c2c3ac5be3c291f2a68e41
Mariam Ali <alim12@csp.edu>
2021-02-02 15:10:47 -0600

    changed server.js

--- server.js
+++ server.js
@@ -3,206 +3,225 @@ const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "weekend",
-    database: "employee_db"
-}); 
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "weekend",
+  database: "employee_db",
+});
 
-connection.connect(function(err) {
-    if (err) throw err;
-    runSearch();
-  });
-  
-  function runSearch(){
-    inquirer.prompt ([{
-        type: "list", 
+connection.connect(function (err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
         message: "What would you like to do?",
         name: "start",
         choices: [
-        "View all Departments",
-        "View all Role",
-        "View all Employees",
-        "Add Department",
-        "Add Role",
-        "Add Employee", 
-        "Update Employee Role", 
-        "Remove Employee",
-        "Exit"
-        ]}
+          "View all Departments",
+          "View all Role",
+          "View all Employees",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employee Role",
+          "Remove Employee",
+          "Exit",
+        ],
+      },
     ])
-    .then(function(answer){
-      switch (answer.start){
-  
+    .then(function (answer) {
+      switch (answer.start) {
         case "View all Departments":
-        viewDepartment();
-        break;
+          viewDepartment();
+          break;
 
-        case "View all Role": 
-        viewRoles(); 
-        break;
+        case "View all Role":
+          viewRoles();
+          break;
 
         case "View all Employees":
-        viewEmployees();
-        break; 
+          viewEmployees();
+          break;
 
-        case "Add Department": 
-        addDepartment(); 
-        break;
+        case "Add Department":
+          addDepartment();
+          break;
 
-        case "Add Roles": 
-        addRole(); 
-        break;
-  
+        case "Add Role":
+          addRole();
+          break;
+
         case "Add Employee":
-        addEmployee();
-        break;
+          addEmployee();
+          break;
 
         case "Update Employee Role":
-        updateEmployeeRole(); 
-        break;
-        
-        case "Remove Employee": 
-        removeEmployee(); 
-        break;
-  
+          updateEmployeeRole();
+          break;
+
+        case "Remove Employee":
+          removeEmployee();
+          break;
+
         case "Exit":
-        connection.end(); 
-        break; 
+          connection.end();
+          break;
       }
-    })
-}
-
-function viewDepartment(){
-    connection.query("SELECT * FROM departments", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      runSearch();
     });
 }
 
-function viewRoles(){
-    connection.query("SELECT * FROM roles", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      runSearch();
-    });
-}
+// function viewDepartment(){
+//     connection.query("SELECT * FROM departments", function(err, res) {
+//       if (err) throw err;
+//       console.table(res);
+//       runSearch();
+//     });
+// }
 
-function viewEmployees(){
-  connection.query("SELECT * FROM employees", function(err, res) {
-    if (err) throw err;
-    console.table(res);
-    runSearch();
-  });
-}
+// function viewRoles(){
+//     connection.query("SELECT * FROM roles", function(err, res) {
+//       if (err) throw err;
+//       console.table(res);
+//       runSearch();
+//     });
+// }
 
-function addDepartment(){
-    inquirer.prompt([{
+// function viewEmployees(){
+//   connection.query("SELECT * FROM employees", function(err, res) {
+//     if (err) throw err;
+//     console.table(res);
+//     runSearch();
+//   });
+// }
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "What is the department name?",
-        name: "deptName"
-        }]).then(function(answer){
-        connection.query("INSERT INTO departments (name) VALUES (?)", [answer.deptName], function(err, res) {
-            if (err) throw err;
-            console.table(res);
-            runSearch();
-        })
-})
+        name: "deptName",
+      },
+    ])
+    .then(function (res) {
+      connection.query(
+        "INSERT INTO departments SET ? ",
+        { name: res.deptName },
+        function (err, res) {
+          if (err) throw err;
+          console.table(res);
+          runSearch();
+        }
+      );
+    });
 }
 
-function addRole(){
-    inquirer.prompt([
-    {
+function addRole() {
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "What is the Role?",
-        name: "roleName"
-    },
-    {
+        name: "roleName",
+      },
+      {
         type: "input",
         message: "What is the salary for this position?",
-        name: "roleSalary"
-    },
-    {
+        name: "roleSalary",
+      },
+      {
         type: "input",
         message: "What is the department ID number?",
-        name: "deptID"
-    }
-
+        name: "deptID",
+      },
     ])
-    .then(function(answer){
-        connection.query("INSERT INTO roles (title, salary, department_id) VALUES (?,?,?,?)", [answer.roleName, answer.roleSalary, answer.deptID], function(err,res) {
-            if (err) throw err;
-            console.table(res);
-            runSearch();
-        })
-    })
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO roles SET ?",
+        {
+          title: answer.roleName,
+          salary: answer.roleSalary,
+          department_id: answer.deptID,
+        },
+        function (err, answer) {
+          if (err) throw err;
+          console.table(answer);
+        }
+      );
+      runSearch();
+    });
 }
 
-function addEmployee(){
-    inquirer.prompt([
-    {
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
         type: "input",
         message: "What is the employee's first name?",
-        name: "firstName"
-    },
-    {
+        name: "firstName",
+      },
+      {
         type: "input",
         message: "What is the employee's last name?",
-        name: "lastName"
-    },
-    {
+        name: "lastName",
+      },
+      {
         type: "input",
         message: "What is this employee's role ID?",
-        name: "roleID"
-    },
-    {
+        name: "roleID",
+      },
+      {
         type: "input",
         message: "What is the manager's ID number?",
-        name: "managerID"
-    }
-
-    ]).then(function(answer){
-        connection.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUE (?,?,?,?)", [answer.firstName, answer.lastName, answer.roleID, answer.managerID], function(err,res) {
-            if (err) throw err;
-            console.table(res);
-            runSearch();
-        })
-    })
+        name: "managerID",
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUE (?,?,?,?)",
+        [answer.firstName, answer.lastName, answer.roleID, answer.managerID],
+        function (err, res) {
+          if (err) throw err;
+          console.table(res);
+          runSearch();
+        }
+      );
+    });
 }
-   
-function updateEmployeeRole(){ 
-    inquirer.prompt([
+
+function updateEmployeeRole() {
+  inquirer.prompt([
     {
-        type: "input",
-        message: "What is the ID of the employee whose role is being chamged?",
-        name: "employeeID"
-                
+      type: "input",
+      message: "What is the ID of the employee whose role is being chamged?",
+      name: "employeeID",
     },
     {
-        type: "input",
-        message: "What is the ID of their new role?",
-        name: "newRoleID"
-    }
-
-    ])
-    // .then(function(data){
-    //    
-    // })
+      type: "input",
+      message: "What is the ID of their new role?",
+      name: "newRoleID",
+    },
+  ]);
+  // .then(function(data){
+  //
+  // })
 }
 
-function removeEmployee(){
-    inquirer.prompt([
+function removeEmployee() {
+  inquirer.prompt([
     {
-        type: "input",
-        message: 'What is the ID of the employee that is being removed?',
-        name: 'employeeInfo'
-    }
-    ])
-    // .then(function(data){
-      
-    // })
-}
+      type: "input",
+      message: "What is the ID of the employee that is being removed?",
+      name: "employeeInfo",
+    },
+  ]);
+  // .then(function(data){
 
-runSearch(); 
+  // })
+}
